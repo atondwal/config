fb0779b88e31154475b43f43d52f0b6bcd998e02
Anish Tondwalkar <anish@archlinux.info>
2017-05-15 18:00:02 -0700

    path fixes

--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ ln -s `pwd`/bin ~/bin
 mkdir -p ~/.config/nvim/
 ln -s `pwd`/init.vim ~/.config/nvim/init.vim
 mkdir -p ~/.emacs/
-ln -s `pwd`/init.el ~/init.el
+ln -s `pwd`/init.el ~/.emacs/init.el
 ln -s `pwd`/.xmonad ~/.xmonad
 ln -s `pwd`/.conkyrc ~/.conkyrc
 ln -s `pwd`/.Xdefaults ~/.Xdefaults
 ln -s `pwd`/.urxvt-perl ~/.urxvt-perl
 
 git clone https://github.com/windelicato/dotfiles windelicato
-ln -s ~/src/windelicato/.colors ~/.colors
+ln -s `pwd`/windelicato/.colors ~/.colors
 
 git clone --recursive https://github.com/atondwal/prezto.git ".zprezto"
 setopt EXTENDED_GLOB
