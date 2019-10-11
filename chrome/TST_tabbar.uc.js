842468a861e5ad3b7858b52bead8939b08d2d564
Anish Tondwalkar <anish@archlinux.info>
2019-10-11 03:05:04 -0700

    tweaks to firefox

--- chrome/TST_tabbar.uc.js
+++ chrome/TST_tabbar.uc.js
@@ -6,19 +6,23 @@
 
     const tabbar = document.getElementById("TabsToolbar");
     const navbar = document.getElementById("nav-bar");
+    const navbox = document.getElementById("navigator-toolbox");
     function showHideTabbar() {
         const sidebarBox = document.getElementById("sidebar-box");
         const sidebarCommand = sidebarBox.getAttribute("sidebarcommand");
+        navbox.style.marginLeft = ( sidebarBox.hidden ? "0" : sidebarBox.width + "px" );
         navbar.style.marginRight = (
             (!sidebarBox.hidden && sidebarCommand === "treestyletab_piro_sakura_ne_jp-sidebar-action") ? "0" : "60vw");
         tabbar.style.opacity = (
             (!sidebarBox.hidden && sidebarCommand === "treestyletab_piro_sakura_ne_jp-sidebar-action") ? "0" : "1");
+      sidebarBox.style.marginTop = "-" + navbox.clientHeight + "px";
     }
     const observer = new MutationObserver(showHideTabbar);
     observer.observe(document.getElementById("sidebar-box"), {
         attributes: true,
         attributeFilter: [
             "sidebarcommand",
+            "width",
             "hidden"]
     });
 }());
