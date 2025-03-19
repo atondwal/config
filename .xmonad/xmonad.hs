db2b089cb00df1a6b57442d554e9765c4101c3dc
Anish Tondwalkar <anish.tondwalkar@gmail.com>
2025-03-19 16:36:28 -0700

    update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,8 +1,9 @@
+{-# LANGUAGE ImplicitParams #-}
 import XMonad.Util.Run
 import XMonad.Util.Replace
 
 import Data.Map (Map, union, fromList)
-import Data.List (nub)
+import Data.List (nub, isInfixOf)
 
 import XMonad
 import XMonad.Config.Mate                  (mateConfig, desktopLayoutModifiers)
@@ -40,30 +41,32 @@ import XMonad.Util.Paste
 import Data.Char
 
 
+import Data.IORef
+
+
 myStartupHook = do
   --dynFlags <- runGhc GHC.getSessionDynFlags
   -- getEnv "PATH" >>= safeSpawn "/usr/bin/xmessage" . pure . show
   -- runAOrWriteError
   -- spawn "conky"
-  -- preload st into RAM :)
-  spawn "mlterm -N term2"
-  spawn "pgrep firefox || firefox"
+  spawn "pgrep firefox || (firefox -P dmodel & firefox -P default-release)"
+  spawn "pgrep mate-panel || mate-panel"
   spawn "setxkbmap -option caps:escape"
-  spawn "compton"
-  spawn "fbautostart"
+  spawn "pgrep picom || picom"
+  spawn "xrandr -r 60.00"
+  -- spawn "fbautostart"
   spawn "sh ~/.fehbg"
   -- spawn "synclient MaxTapTime=0"
-  spawn "/usr/lib/kdeconnectd"
+  -- spawn "/usr/lib/kdeconnectd"
   spawn "/usr/lib/notify-osd/notify-osd"
   -- spawn "redshift"
-  spawn "pgrep chrome || google-chrome"
-  spawn "pgrep firefox || firefox"
+  -- spawn "pgrep chrome || google-chrome"
   io. print =<< mapM (runQuery className) =<< gets (W.allWindows . windowset)
 
 main :: IO ()
 main = do
-  replace
-  spawn "gcertstatus || mlterm -N term2 -e mcert"
+  ref <- newIORef 6500
+  let ?temp = ref
   xmonad $ mateConfig {
       terminal   = "mlterm"
     , modMask    = mod4Mask -- Super
@@ -99,20 +102,27 @@ scratchpads =
   , NS "term2" "mlterm -N term2" (className =? "term2") (geo (1/3) (2/3) (1/3) (1/3))
   , NS "term3" "mlterm -N term3 -w 26" (className  =? "term3") (geo (2/3) (2/3) (1/3) (1/3))
   , NS "term4" "mlterm -N term4" (className  =? "term4") (geo (7/9) (1/10) (3/18) (1/10))
-  , NS "ranger" "mlterm -N ranger -e ranger" (className =? "ranger") (geo (1/5) (1/5) (7/10) (7/10))
+  , NS "logseq" "logseq" (className =? "Logseq" <&&> (not . isInfixOf "pdf" <$> stringProperty "WM_NAME")) (geo (1/5) (1/5) (7/10) (7/10))
   , NS "pulsemixer" "mlterm -N pulsemixer -e pulsemixer" (className  =? "pulsemixer")  (geo 0 (2/3) (1/3) (1/3))
   , NS "htop" "mlterm -N htop -e htop" (className  =? "htop")  (geo (1/3) (2/3) (1/3) (1/3))
   ]
  where
   geo a b c d = customFloating (RationalRect a b c d)
 
-myMouse :: XConfig y -> Map (ButtonMask, Button) (Window -> X ())
+myMouse :: (?temp :: IORef Int) => XConfig y -> Map (ButtonMask, Button) (Window -> X ())
 myMouse XConfig{modMask = m, terminal = term} = fromList [
-    ((m               , button4)         , updateOpacity (+ 0.1))
-  , ((m               , button5)         , updateOpacity (subtract 0.1))
-  , ((m .|. shiftMask , button5)         , \ _ -> spawn "lux -s 3%")
-  , ((m .|. shiftMask , button4)         , \ _ -> spawn "lux -a 3%")
+    ((m               , button4)                  , \ _ -> updateTemp (+ 50))
+  , ((m               , button5)                  , \ _ -> updateTemp (subtract 50))
+  , ((m .|. shiftMask , button5)                  , \ _ -> spawn "lux -s 3%")
+  , ((m .|. shiftMask , button4)                  , \ _ -> spawn "lux -a 3%")
   ]
+
+updateTemp :: (?temp :: IORef Int) => (Int -> Int) -> X ()
+updateTemp f = do
+    io $ modifyIORef ?temp f
+    temp <- io $ readIORef ?temp
+    -- spawn $ "notify-send " ++ (show temp)
+    spawn $ "redshift -r -P -O" ++ (show temp)
 
 myKeys :: XConfig y -> Map (KeyMask, KeySym) (X ())
 myKeys XConfig{modMask = m, terminal = term, workspaces = sps} = fromList $ [
@@ -145,16 +155,19 @@ myKeys XConfig{modMask = m, terminal = term, workspaces = sps} = fromList $ [
   , ((m, xK_s), S.namedScratchpadAction scratchpads "term2")
   , ((m .|. shiftMask, xK_s), S.namedScratchpadAction scratchpads "htop")
   , ((m, xK_d), S.namedScratchpadAction scratchpads "term3")
-  , ((m, xK_o), S.namedScratchpadAction scratchpads "ranger")
+  , ((m, xK_o), S.namedScratchpadAction scratchpads "logseq")
 
   , ((m, xK_z), spawn "xcalib -i -a")
   , ((m .|. shiftMask, xK_o), spawn "thunar")
+  -- , ((m              , xK_p), shellPrompt (greenXPConfig {historyFilter = nub}))
   , ((m              , xK_p), shellPrompt (greenXPConfig {historyFilter = nub}))
+  , ((m              , xK_r), spawn  "rofi -show run")
+  , ((m .|. shiftMask, xK_r), spawn  "rofi -show drun")
 
   , ((0, xF86XK_MonBrightnessUp),              spawn "ibacklight -dec 10")
   , ((0, xF86XK_MonBrightnessDown),            spawn "ibacklight -inc 10")
 
-  , ((m, xK_n),               getSelection >>= spawn . (++"\"") . ("xdotool type --clearmodifiers -- \"" ++) . capsarcasm)
+  -- , ((m, xK_n),               getSelection >>= spawn . (++"\"") . ("xdotool type --clearmodifiers -- \"" ++) . capsarcasm)
   -- , ((m, xK_m),               spawn "mpv `xclip -o`")
   , ((m .|. mod1Mask, xK_w),  spawn "sh ~/bin/wall.sh")
   , ((m .|. mod1Mask, xK_d),  spawn "sh ~/bin/floor.sh" >> spawn "sh ~/bin/wall.sh")
@@ -163,7 +176,8 @@ myKeys XConfig{modMask = m, terminal = term, workspaces = sps} = fromList $ [
     (act, mod) <- [(shift, shiftMask), (copy, shiftMask .|. controlMask)]
     [((m .|. mod, k), windows $ act sp)]
  where
-  dmenu = "exe=`dmenu_path | yeganesh -x -- -i -b -sb \"#689d6a\" -sf \"#2d2d2d\" -nb \"#2d2d2d\" -nf grey -fn 'Source Code Pro-9'` && eval \"$exe\""
+  -- dmenu = "exe=`dmenu_path | yeganesh -- -i -b -sb \"#689d6a\" -sf \"#2d2d2d\" -nb \"#2d2d2d\" -nf grey -fn 'Source Code Pro-16'` && eval \"$exe\""
+  dmenu = "rofi -combi-modi window,run,drun -show combi"
 
   capsarcasm (x:y:xs) = toUpper x : y : capsarcasm xs
   capsarcasm xs = xs
